3191a04d9839475d96444288f29a29266b058750
Michael Utz <michael@theutz.com>
2024-10-08 13:10:34 +0300

    chore: add groups
    Signed-off-by: Michael Utz <michael@theutz.com>

--- config/plugins/which-key.nix
+++ config/plugins/which-key.nix
@@ -1,17 +1,28 @@
 {lib, ...}: let
   inherit (lib) map elemAt mapAttrsToList;
+
+  groups = {
+    "<tab>" = "Tabs";
+    "b" = "Buffers";
+    "g" = "Git";
+    "q" = "Quit/session";
+    "s" = "Search";
+    "w" = "Window";
+  };
+
+  mkGroup = key: group: {
+    inherit group;
+    __unkeyed-1 = "<leader>${key}";
+  };
+
+  groupSpec = mapAttrsToList mkGroup groups;
 in {
   plugins.which-key = {
     enable = true;
+
     settings = {
       preset = "modern";
-      spec = [
-        {
-          mode = ["n"];
-          __unkeyed-1 = "<leader>s";
-          group = "Search";
-        }
-      ];
+      spec = groupSpec;
     };
   };
 }
