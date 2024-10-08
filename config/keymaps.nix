9e94a0d6639e9921a318bdb2ae1ce45252009d0c
Michael Utz <michael@theutz.com>
2024-10-08 14:43:57 +0300

    refactor: handwritten maps
    Signed-off-by: Michael Utz <michael@theutz.com>

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -2,55 +2,197 @@
   inherit (lib) mapAttrsToList;
   inherit (builtins) map elemAt;
 
-  winKeyMapper = key: desc: {
-    key = "<leader>w${key}";
-    mode = ["n"];
-    action = "<cmd>wincmd ${key}<cr>";
-    options.desc = desc;
-  };
-  winCmds = {
-    "+" = "Increase height";
-    "-" = "Decrease height";
-    "<" = "Decrease width";
-    "=" = "Equal width and height";
-    ">" = "Increase width";
-    "_" = "Max out the height";
-    "h" = "Focus left window";
-    "j" = "Focus window below";
-    "k" = "Focus window above";
-    "l" = "Focus right window";
-    "o" = "Close all others";
-    "q" = "Close window";
-    "s" = "Split window horizontally";
-    "T" = "Break into new tab";
-    "v" = "Split window vertically";
-    "w" = "Switch windows";
-    "x" = "Swap current with next";
-    "|" = "Max out the width";
-  };
-  winKeys = mapAttrsToList winKeyMapper winCmds;
-
-  leaderMapper = arg: let
-    key = elemAt arg 0;
-    cmd = elemAt arg 1;
-    desc = elemAt arg 2;
-  in {
-    mode = ["n"];
-    key = "<leader>${key}";
-    action = "<cmd>${cmd}<cr>";
-    options.desc = desc;
-  };
-  leaderCmds = [
-    ["bd" "bd" "Delete buffer"]
-    ["bD" "bd!" "Delete buffer (force)"]
-    ["bp" "bp" "Previous buffer"]
-    ["bn" "bn" "Next buffer"]
+  winKeys = [
+    {
+      mode = ["n"];
+      key = "<leader>w+";
+      action = "<cmd>wincmd +<cr>";
+      options.desc = "Increase height";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w-";
+      action = "<cmd>wincmd -<cr>";
+      options.desc = "Decrease height";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w<";
+      action = "<cmd>wincmd <<cr>";
+      options.desc = "Decrease width";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w=";
+      action = "<cmd>wincmd =<cr>";
+      options.desc = "Equal width and height";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w>";
+      action = "<cmd>wincmd ><cr>";
+      options.desc = "Increase width";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w_";
+      action = "<cmd>wincmd _<cr>";
+      options.desc = "Max out the height";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wh";
+      action = "<cmd>wincmd h<cr>";
+      options.desc = "Focus left window";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wj";
+      action = "<cmd>wincmd j<cr>";
+      options.desc = "Focus window below";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wk";
+      action = "<cmd>wincmd k<cr>";
+      options.desc = "Focus window above";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wl";
+      action = "<cmd>wincmd l<cr>";
+      options.desc = "Focus right window";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wo";
+      action = "<cmd>wincmd o<cr>";
+      options.desc = "Close all others";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wq";
+      action = "<cmd>wincmd q<cr>";
+      options.desc = "Close window";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>ws";
+      action = "<cmd>wincmd s<cr>";
+      options.desc = "Split window horizontally";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wT";
+      action = "<cmd>wincmd T<cr>";
+      options.desc = "Break into new tab";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wv";
+      action = "<cmd>wincmd v<cr>";
+      options.desc = "Split window vertically";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>ww";
+      action = "<cmd>wincmd w<cr>";
+      options.desc = "Switch windows";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>wx";
+      action = "<cmd>wincmd x<cr>";
+      options.desc = "Swap current with next";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>w|";
+      action = "<cmd>wincmd |<cr>";
+      options.desc = "Max out the width";
+    }
   ];
-  leaderKeys = map leaderMapper leaderCmds;
+
+  bufferKeys = [
+    {
+      mode = ["n"];
+      key = "<leader>bd";
+      action = "<cmd>bd<cr>";
+      options.desc = "Delete buffer";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>bD";
+      action = "<cmd>bd!<cr>";
+      options.desc = "Delete buffer (force)";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>bp";
+      action = "<cmd>bp<cr>";
+      options.desc = "Previous buffer";
+    }
+    {
+      mode = ["n"];
+      key = "<leader>bn";
+      action = "<cmd>bn<cr>";
+      options.desc = "Next buffer";
+    }
+    {
+      mode = ["n"];
+      key = "[b";
+      action = "<cmd>bp<cr>";
+      options = {
+        desc = "Previous buffer";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "]b";
+      action = "<cmd>bn<cr>";
+      options = {
+        desc = "Next buffer";
+      };
+    }
+  ];
+
+  tabKeys = [
+    {
+      mode = ["n"];
+      key = "<leader><tab>n";
+      action = "<cmd>tabnext<cr>";
+      options.desc = "Next tab";
+    }
+    {
+      mode = ["n"];
+      key = "<leader><tab>]";
+      action = "<cmd>tabnext<cr>";
+      options.desc = "Next tab";
+    }
+    {
+      mode = ["n"];
+      key = "<leader><tab>p";
+      action = "<cmd>tabprev<cr>";
+      options.desc = "Prev tab";
+    }
+    {
+      mode = ["n"];
+      key = "<leader><tab>[";
+      action = "<cmd>tabprev<cr>";
+      options.desc = "Prev tab";
+    }
+    {
+      mode = ["n"];
+      key = "<leader><tab><space>";
+      action = "<cmd>wincmd T<cr>";
+      options.desc = "Open in new tab";
+    }
+  ];
 in {
   keymaps =
     winKeys
-    ++ leaderKeys
+    ++ bufferKeys
+    ++ tabKeys
     ++ [
       {
         mode = ["n"];
@@ -68,55 +210,9 @@ in {
       }
       {
         mode = ["n"];
-        key = "[b";
-        action = "<cmd>bp<cr>";
-        options = {
-          desc = "Previous buffer";
-        };
-      }
-      {
-        mode = ["n"];
-        key = "]b";
-        action = "<cmd>bn<cr>";
-        options = {
-          desc = "Next buffer";
-        };
-      }
-      {
-        mode = ["n"];
         key = "<leader>qq";
         action = "<cmd>xa<cr>";
         options.desc = "Write all and quit";
-      }
-      {
-        mode = ["n"];
-        key = "<leader><tab>n";
-        action = "<cmd>tabnext<cr>";
-        options.desc = "Next tab";
-      }
-      {
-        mode = ["n"];
-        key = "<leader><tab>]";
-        action = "<cmd>tabnext<cr>";
-        options.desc = "Next tab";
-      }
-      {
-        mode = ["n"];
-        key = "<leader><tab>p";
-        action = "<cmd>tabprev<cr>";
-        options.desc = "Prev tab";
-      }
-      {
-        mode = ["n"];
-        key = "<leader><tab>[";
-        action = "<cmd>tabprev<cr>";
-        options.desc = "Prev tab";
-      }
-      {
-        mode = ["n"];
-        key = "<leader><tab><space>";
-        action = "<cmd>wincmd T<cr>";
-        options.desc = "Open in new tab";
       }
     ];
 }
