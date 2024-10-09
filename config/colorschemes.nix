4849aaf3a0e7ab8d6e64395172b825fe16eb8ff7
Michael Utz <michael@theutz.com>
2024-10-09 23:31:34 +0300

    chore: fix color scheme
    Signed-off-by: Michael Utz <michael@theutz.com>

--- config/colorschemes.nix
+++ config/colorschemes.nix
@@ -1,9 +1,15 @@
-{...}: {
+{
   colorschemes.tokyonight = {
     enable = true;
     settings = {
-      style = "moon";
+      day_brightness = 0.3;
+      style = "night"; # moon, storm, night, or day
       light_style = "day";
+      dim_inactive = true;
+      hide_inactive_statusline = true;
+      lualine_bold = true;
+      terminal_colors = true;
+      transparent = true;
     };
   };
 }
