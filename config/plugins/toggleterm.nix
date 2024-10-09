0527f7e27261b53241a6a1256c3aca2a2fbdb860
Michael Utz <michael@theutz.com>
2024-10-09 15:45:23 +0300

    fix: toggleterm
    Signed-off-by: Michael Utz <michael@theutz.com>

--- config/plugins/toggleterm.nix
+++ config/plugins/toggleterm.nix
@@ -2,7 +2,9 @@
   plugins.toggleterm = {
     enable = true;
     settings = {
-      open_mapping = "[[<C-/>]]";
+      open_mapping = "[[<C-_>]]";
+      insert_mappings = true;
+      terminal_mappings = true;
     };
   };
 }
